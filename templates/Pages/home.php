394899298909c14272d972a8950667b1ddf6b2e6
ForSureN1 <standin1532@gmail.com>
2024-04-01 11:38:12 +0500

    Update home.php

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -503,7 +503,7 @@
                                 </div>
                             <?php endif; ?> -->
                             <a href="https://www.youtube.com/@elordaaqparat2341" target="_blank" class="widget__youtube-header">
-                                <img src="../img/el-h-2.png" alt="">
+                                <img src="../img/el-h-2.jpg" alt="">
                             </a>
                             <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Sn_8FJkgANI?si=0_41jzCRuncKqOWL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                             <!-- <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script> -->
