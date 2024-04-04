701cfab97e4fb8ee804ff4e035938cce7397fa5b
backend2-ac <backend2@astana-creative.kz>
2024-04-04 17:57:00 +0500

    fix counter

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -73,36 +73,32 @@
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    
+
                     gtag('config', 'G-14414JMZLQ');
                     </script>
-                    <!-- Yandex.Metrika informer -->
-                        <a href="https://metrika.yandex.ru/stat/?id=45707448&amp;from=informer"
-                            target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/45707448/3_1_FFFFFFFF_EFEFEFFF_0_visits"
-                            style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="45707448" data-lang="ru" />
-                        </a>
-                    <!-- /Yandex.Metrika informer -->
+
                     <!-- Yandex.Metrika counter -->
                     <script type="text/javascript" >
-                       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                       m[i].l=1*new Date();
-                       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                    
-                       ym(95942695, "init", {
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                        ym(95942695, "init", {
                             clickmap:true,
                             trackLinks:true,
                             accurateTrackBounce:true,
                             webvisor:true
-                       });
+                        });
                     </script>
                     <noscript><div><img src="https://mc.yandex.ru/watch/95942695" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
                     <!-- /Yandex.Metrika counter -->
+
                     <!--LiveInternet counter-->
                     <a href="https://www.liveinternet.ru/click"
                         target="_blank">
-                        <img id="licnt1443" width="88" height="31" style="border:0" 
+                        <img id="licnt1443" width="88" height="31" style="border:0"
                         title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня"
                         src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
                         alt=""/>
