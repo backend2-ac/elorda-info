003ba2f132cbab07ac11b49ad035bc2f467ddd7a
ForSureN1 <standin1532@gmail.com>
2024-04-09 18:46:09 +0500

    time

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -47,8 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
         headerTime.innerHTML = hours + ':' + minutes;
     }
 
-    // Вызываем функцию сразу после установки интервала
-    updateHeaderTime();
+    if(headerTime) {
+        headerTime.classList.add('active');
+        // Вызываем функцию сразу после установки интервала
+        updateHeaderTime();
+    }
 
     // Устанавливаем интервал на повторение каждую минуту
     setInterval(updateHeaderTime, 60000);
