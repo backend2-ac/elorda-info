76f3e2c0b814e3907189e017198316576363b7e1
backend2-ac <backend2@astana-creative.kz>
2024-05-02 12:40:55 +0500

    телега

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -384,7 +384,7 @@
                             <div class="widget__tg">
                                 <div class="widget__tg-block">
                                     <a href="<?= $comps[8]['body'] ?>" target="_blank" class="widget__tg-header">
-                                        <?= __('Популярное в нашем Telegram-канале') ?>
+                                        <?= __('Последние в нашем Telegram-канале') ?>
                                         <img src="/img/tg-widget-icon.png" alt="">
                                     </a>
                                     <div class="widget__tg-items">
@@ -393,7 +393,7 @@
                                                 <div class="widget__tg-title"><?= $tg_post['title'] ?></div>
                                                 <div class="widget__tg-elems">
                                                     <div class="widget__tg-elem">
-                                                        <?= date('d.m.Y | H:m', $tg_post['date']) ?>
+<!--                                                        --><?php //= date('d.m.Y | H:m', $tg_post['date']) ?>
                                                     </div>
                                                 </div>
                                             </a>
