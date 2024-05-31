484329360ed03c6d1f11bceaa9678d98e8977f84
backend2-ac <backend2@astana-creative.kz>
2024-05-31 18:53:10 +0500

    youtube

--- templates/element/youtube.php
+++ templates/element/youtube.php
@@ -1,14 +1,14 @@
 <div class="widget__youtube">
-    <!-- <div class="widget__youtube-header">
-                                <img src="/img/comps/<?=$comps[17]['img']?>" alt="">
-                            </div>
-                            <?php if (isset($comps[18]['body'])): ?>
-                                <div class="widget__youtubte-frame">
-                                    <?=$comps[18]['body']?>
-                                </div>
-                            <?php endif; ?> -->
+<!--    <div class="widget__youtube-header">-->
+<!--                                <img src="/img/comps/--><?php //=$comps[17]['img']?><!--" alt="">-->
+<!--                            </div>-->
+<!--                            --><?php //if (isset($comps[18]['body'])): ?>
+<!--                                <div class="widget__youtubte-frame">-->
+<!--                                    --><?php //=$comps[18]['body']?>
+<!--                                </div>-->
+<!--                            --><?php //endif; ?>
     <a href="https://www.youtube.com/@elordaaqparat2341" target="_blank" class="widget__youtube-header">
-        <img src="../img/el-h-2.jpg" alt="">
+        <img src="/img/el-h-2.jpg" alt="">
     </a>
     <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Sn_8FJkgANI?si=0_41jzCRuncKqOWL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     <!-- <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script> -->
